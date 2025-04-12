a2771bae15c77b42b8198274d5b0c64ca1cb17ef
Raktimava Bhattacharyya <raktim.bhatta29@gmail.com>
2025-04-13 02:52:55 +0530

    Notepad added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,7 @@
-import { Box, Text } from "@chakra-ui/react";
+import Whiteboard from "./components/Whiteboard";
 
-function App(){
-  
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">My React App</h1>
-      <Box bg="black" className="size-1/5">
-        <Text className="text-white">Hello</Text>
-      </Box>
-    </div>
-  );
+function App() {
+  return <Whiteboard />;
 }
 
 export default App;
